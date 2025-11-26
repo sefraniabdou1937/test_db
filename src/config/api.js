8dd05ccfbb7f01f57de9fb95fbb8a95e16b6a6c1
ABDOU <abderehmensef00@gmail.com>
2025-11-26 19:14:40 +0100

    test_utl

--- src/config/api.js
+++ src/config/api.js
@@ -3,10 +3,10 @@
 // --- MODIFICATION CRUCIALE ---
 // On laisse vide pour forcer l'usage du chemin relatif sur Azure.
 // En local, si React et FastAPI sont sur le même port (via le "Monolithe"), ça marchera aussi.
-export const API_BASE_URL = ""; 
+export const API_URL = ""; 
 
 export const getEndpoint = (path) => {
   // Nettoie le chemin pour éviter les doubles slashs (//)
   const cleanPath = path.startsWith('/') ? path : `/${path}`;
-  return `${API_BASE_URL}${cleanPath}`;
+  return `${API_URL}${cleanPath}`;
 };
